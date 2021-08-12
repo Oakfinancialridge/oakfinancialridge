ac2775a4b176ae72d28e232fe83297916a3236e5
Oakfinancialridge <88825923+Oakfinancialridge@users.noreply.github.com>
2021-08-12 11:30:26 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ var password = document.getElementById("password");
 var login1 = document.getElementById("login1");
 
 login1.addEventListener('click', () => {
-    if (userName.value == "gl1692830@gmail.com" && password.value == "samuel2020") {
+    if (userName.value == "gl1692830@gmail.com" && password.value == "Gathib09$") {
         return window.location.href = "https://oakfinancialridge.github.io/account/";
     } else {
         alert("Sorry, invalid details..");
