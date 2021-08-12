5491feead51fdd852902a3f3b77601993fd0679c
Oakfinancialridge <88825923+Oakfinancialridge@users.noreply.github.com>
2021-08-12 11:09:54 +0100

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,18 @@
+var userName = document.getElementById("username");
+var password = document.getElementById("password");
+
+var login1 = document.getElementById("login1");
+
+login1.addEventListener('click', () => {
+    if (userName.value == "gl1692830@gmail.com" && password.value == "samuel2020") {
+        return window.location.href = "https://oakfinancialridge.github.io/account/";
+    } else {
+        alert("Sorry, invalid details..");
+        return false;
+    }
+});
+
+//VALIDATOR..
+function auth() {
+
+}
